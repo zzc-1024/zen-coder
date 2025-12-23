5898a4e836ed53ed5f0da3a9cbc130d039ff2dd1
周子程 <1977309465@qq.com>
2025-12-23 23:40:01 +0800

    ✏️ 旷日持久的笔误: 又是 get 和 set 写错了的情况

--- apps/web/src/nodes/basic/getNode/GetNodeView.vue
+++ apps/web/src/nodes/basic/getNode/GetNodeView.vue
@@ -20,7 +20,7 @@ import { ref, inject, onMounted } from 'vue';
 import { EventType, GraphModel } from '@logicflow/core';
 import NodeHeader from '@/components/node/NodeHeader.vue';
 import type SetNodeModel from './getNodeModel';
-import type { SetNodeProperties } from './getNodeModel';
+import type { GetNodeProperties } from './getNodeModel';
 import NodeField from '@/components/node/NodeField.vue';
 
 // 1. 注入 LogicFlow 核心方法
@@ -29,7 +29,7 @@ const getGraph = inject<() => GraphModel>('getGraph');
 
 // 2. 定义响应式数据
 // 给定一个默认结构，防止渲染报错
-const properties = ref<SetNodeProperties>();
+const properties = ref<GetNodeProperties>();
 
 // 3. 初始化与事件监听
 onMounted(() => {
@@ -39,7 +39,7 @@ onMounted(() => {
   const graph = getGraph();
 
   // --- 函数：从节点属性同步数据到 Vue 组件 ---
-  const updateData = (props: SetNodeProperties) => {
+  const updateData = (props: GetNodeProperties) => {
     // 这里做一层合并，确保即使 props 为空也有默认值
     properties.value = {
       title: props.title,
@@ -48,7 +48,7 @@ onMounted(() => {
   };
 
   // A. 初始化：首次加载时读取数据
-  updateData(node.properties as SetNodeProperties);
+  updateData(node.properties as GetNodeProperties);
 
   // B. 监听：当图表上的节点属性发生变化时（例如通过 properties 面板修改了表名）
   graph.eventCenter.on(
@@ -56,7 +56,7 @@ onMounted(() => {
     (eventData: { id: string; properties: unknown }) => {
       // 关键判断：确保事件是针对当前这个节点的
       if (eventData.id !== node.id) return;
-      updateData(eventData.properties as SetNodeProperties);
+      updateData(eventData.properties as GetNodeProperties);
     },
   );
 });
