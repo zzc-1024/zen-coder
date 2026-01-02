0e7e51d34c57d92756b17f2bf8a966a8b5c5ebf8
周子程 <1977309465@qq.com>
2026-01-03 00:01:48 +0800

    🚩 立flag: 新增表达式接口设计

---
+++ apps/web/src/parser/defination.ts
@@ -0,0 +1,6 @@
+export type CommonType = string | number | boolean;
+
+export abstract class Expression {
+  // abstract toString(): string
+  // abstract toJSON(): string
+}
---
+++ apps/web/src/parser/expressions/index.ts
@@ -0,0 +1,100 @@
+import { Expression, type CommonType } from '../defination';
+
+export class ConstantExpression extends Expression {
+  constructor(public value: CommonType) {
+    super();
+  }
+}
+
+export class VariableExpression extends Expression {
+  constructor(public name: string) {
+    super();
+  }
+}
+
+export class FunctionCallExpression extends Expression {
+  constructor(
+    public name: string,
+    public args: Expression[],
+  ) {
+    super();
+  }
+}
+
+export class IndexExpression extends Expression {
+  constructor(public base: Expression, public index: Expression) {
+    super();
+  }
+}
+
+export class DictExpression extends Expression {
+  constructor(public pairs: [Expression, Expression][]) {
+    super();
+  }
+}
+
+export class ListExpression extends Expression {
+  constructor(public items: Expression[]) {
+    super();
+  }
+}
+
+export class TupleExpression extends Expression {
+  constructor(public items: Expression[]) {
+    super();
+  }
+}
+
+export class SetExpression extends Expression {
+  constructor(public items: Expression[]) {
+    super();
+  }
+}
+
+export class RangeExpression extends Expression {
+  constructor(public start: Expression, public end: Expression) {
+    super();
+  }
+}
+
+export class UnaryExpression extends Expression {
+  constructor(public operator: string, public operand: Expression) {
+    super();
+  }
+}
+
+export class BinaryExpression extends Expression {
+  constructor(public operator: string, public left: Expression, public right: Expression) {
+    super();
+  }
+}
+
+export class BooleanExpression extends Expression {
+  constructor(public value: boolean) {
+    super();
+  }
+}
+
+export class FloatExpression extends Expression {
+  constructor(public value: number) {
+    super();
+  }
+}
+
+export class IntegerExpression extends Expression {
+  constructor(public value: number) {
+    super();
+  }
+}
+
+export class StringExpression extends Expression {
+  constructor(public value: string) {
+    super();
+  }
+}
+
+export class NoneExpression extends Expression {
+  constructor() {
+    super();
+  }
+}
