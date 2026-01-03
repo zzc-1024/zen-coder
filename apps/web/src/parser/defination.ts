07ccbf1e079bf409cdbb5873cbfb8cf385cc544a
周子程 <1977309465@qq.com>
2026-01-03 11:01:41 +0800

    ✨ 添砖加瓦: 添加了 Statement 的定义

--- apps/web/src/parser/defination.ts
+++ apps/web/src/parser/defination.ts
@@ -2,3 +2,7 @@ export abstract class Expression {
   // abstract toString(): string
   // abstract toJSON(): string
 }
+export abstract class Statement {
+  // abstract toString(): string
+  // abstract toJSON(): string
+}
---
+++ apps/web/src/parser/statements/index.ts
@@ -0,0 +1,203 @@
+import { Expression, Statement } from '../defination';
+import type { VariableExpression } from '../expressions';
+
+export class AssignmentStatement extends Statement {
+  constructor(
+    public variable: VariableExpression,
+    public expression: Expression,
+  ) {
+    super();
+  }
+}
+
+export class PrintStatement extends Statement {
+  constructor(public expression: Expression) {
+    super();
+  }
+}
+
+export class IfStatement extends Statement {
+  constructor(
+    public condition: Expression,
+    public thenStatements: Statement[],
+    public elseStatements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class WhileStatement extends Statement {
+  constructor(
+    public condition: Expression,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class BlockStatement extends Statement {
+  constructor(public statements: Statement[]) {
+    super();
+  }
+}
+
+export class FunctionStatement extends Statement {
+  constructor(
+    public name: string,
+    public parameters: VariableExpression[],
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class ReturnStatement extends Statement {
+  constructor(public expression: Expression) {
+    super();
+  }
+}
+
+export class BreakStatement extends Statement {
+  constructor() {
+    super();
+  }
+}
+
+export class ContinueStatement extends Statement {
+  constructor() {
+    super();
+  }
+}
+
+export class ExpressionStatement extends Statement {
+  constructor(public expression: Expression) {
+    super();
+  }
+}
+
+export class EmptyStatement extends Statement {
+  constructor() {
+    super();
+  }
+}
+
+export class CommentStatement extends Statement {
+  constructor(public comment: string) {
+    super();
+  }
+}
+
+export class LabelStatement extends Statement {
+  constructor(public label: string) {
+    super();
+  }
+}
+
+export class SwitchStatement extends Statement {
+  constructor(
+    public expression: Expression,
+    public cases: CaseClause[],
+  ) {
+    super();
+  }
+}
+
+export class CaseClause extends Statement {
+  constructor(
+    public expression: Expression,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class DefaultClause extends Statement {
+  constructor(public statements: Statement[]) {
+    super();
+  }
+}
+
+export class ThrowStatement extends Statement {
+  constructor(public expression: Expression) {
+    super();
+  }
+}
+
+export class TryStatement extends Statement {
+  constructor(
+    public tryStatements: Statement[],
+    public catchStatements: CatchClause[],
+  ) {
+    super();
+  }
+}
+
+export class CatchClause extends Statement {
+  constructor(
+    public variable: VariableExpression,
+    public catchStatements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class FinallyClause extends Statement {
+  constructor(public finallyStatements: Statement[]) {
+    super();
+  }
+}
+
+export class DebuggerStatement extends Statement {
+  constructor() {
+    super();
+  }
+}
+
+export class WithStatement extends Statement {
+  constructor(
+    public object: Expression,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class ForStatement extends Statement {
+  constructor(
+    public initializer: ExpressionStatement,
+    public condition: Expression,
+    public updater: ExpressionStatement,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class ForInStatement extends Statement {
+  constructor(
+    public left: VariableExpression,
+    public right: Expression,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class ForOfStatement extends Statement {
+  constructor(
+    public left: VariableExpression,
+    public right: Expression,
+    public statements: Statement[],
+  ) {
+    super();
+  }
+}
+
+export class DoWhileStatement extends Statement {
+  constructor(
+    public statements: Statement[],
+    public condition: Expression,
+  ) {
+    super();
+  }
+}
