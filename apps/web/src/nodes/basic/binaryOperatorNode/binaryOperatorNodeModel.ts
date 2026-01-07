39adb96e7af5d8c9088c39efd92d81624cb4b795
周子程 <1977309465@qq.com>
2026-01-07 21:26:46 +0800

    ✨ 添砖加瓦: 实现了双目运算符的全部算子

--- apps/web/src/nodes/basic/binaryOperatorNode/binaryOperatorNodeModel.ts
+++ apps/web/src/nodes/basic/binaryOperatorNode/binaryOperatorNodeModel.ts
@@ -3,13 +3,16 @@ import BasicNodeModel, {
   type BasicNodePropertiesWithDefaultValues,
   type FieldType,
 } from '../basicNodeModel';
-import {
-  BasicEditorNodeTypePrefix,
-  type AnchorType,
-} from '../typeDifination';
+import { BasicEditorNodeTypePrefix, type AnchorType, type DirectType } from '../typeDifination';
 import type LogicFlow from '@logicflow/core';
 import { BinaryExpression, type BinaryOperator } from '@/parser/expressions';
-import { BasicType, BUILTIN_BASIC_FLOAT_TYPE, BUILTIN_BASIC_INTEGER_TYPE, parseType } from '@/parser/variable';
+import {
+  BasicType,
+  BUILTIN_BASIC_BOOLEAN_TYPE,
+  BUILTIN_BASIC_INTEGER_TYPE,
+  BUILTIN_BASIC_STRING_TYPE,
+  parseType,
+} from '@/parser/variable';
 
 export const BinaryOperatorNodeType = `${BasicEditorNodeTypePrefix}:binaryOperator`;
 export type BinaryOperatorNodeProperties = BasicNodePropertiesWithDefaultValues & {
@@ -25,7 +28,7 @@ export const BinaryOperatorNodeAnchorIds = {
 
 class BinaryOperatorNodeModel extends BasicNodeModel {
   getFields(): FieldType[] {
-    return [
+    const fieldType: FieldType[] = [
       {
         name: '左操作数',
         type: parseType(this.properties.type as string),
@@ -38,13 +41,31 @@ class BinaryOperatorNodeModel extends BasicNodeModel {
         inputId: BinaryOperatorNodeAnchorIds.RIGHT_OPERAND,
         outputId: null,
       },
-      {
+    ];
+    const comparisonOperators: BinaryOperator[] = [
+      'greater_than',
+      'greater_than_or_equal',
+      'less_than',
+      'less_than_or_equal',
+      'equal',
+      'not_equal',
+    ];
+    if (comparisonOperators.includes(this.properties.operator as BinaryOperator)) {
+      fieldType.push({
+        name: '计算结果',
+        type: new BasicType('builtin:basic:boolean'),
+        inputId: null,
+        outputId: BinaryOperatorNodeAnchorIds.DATA_OUT,
+      });
+    } else {
+      fieldType.push({
         name: '计算结果',
         type: parseType(this.properties.type as string),
         inputId: null,
         outputId: BinaryOperatorNodeAnchorIds.DATA_OUT,
-      },
-    ];
+      });
+    }
+    return fieldType;
   }
 
   parseFlowIn(anchorId: string): Statement[] {
@@ -91,16 +112,145 @@ class BinaryOperatorNodeModel extends BasicNodeModel {
 
 export function binaryOperatorNodeGenerateAnchorRecommendation(
   anchorType: AnchorType,
+  direction: DirectType,
 ): LogicFlow.OnDragNodeConfig[] {
   if (!(anchorType instanceof BasicType)) {
     return [];
   }
-  if (
-    anchorType.basicTypeName !== BUILTIN_BASIC_INTEGER_TYPE &&
-    anchorType.basicTypeName !== BUILTIN_BASIC_FLOAT_TYPE
-  ) {
-    return [];
-  }
+  // 预先定义要复用的比较运算符
+  const comparisonDragNodeConfig: LogicFlow.OnDragNodeConfig[] = [
+    {
+      type: BinaryOperatorNodeType,
+      label: '大于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'greater_than',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '大于等于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'greater_than_or_equal',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '小于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'less_than',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '小于等于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'less_than_or_equal',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '等于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'equal',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '不等于',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'not_equal',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+  ];
+
+  // 先处理布尔型
+  if (anchorType.basicTypeName === BUILTIN_BASIC_BOOLEAN_TYPE) {
+    const booleanTypeResult: LogicFlow.OnDragNodeConfig[] = [
+      {
+        type: BinaryOperatorNodeType,
+        label: '与',
+        icon: 'favicon.ico',
+        properties: {
+          type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+          operator: 'and',
+          defaultValues: {},
+        } satisfies BinaryOperatorNodeProperties,
+      },
+      {
+        type: BinaryOperatorNodeType,
+        label: '或',
+        icon: 'favicon.ico',
+        properties: {
+          type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+          operator: 'or',
+          defaultValues: {},
+        } satisfies BinaryOperatorNodeProperties,
+      },
+      {
+        type: BinaryOperatorNodeType,
+        label: '不相等xor',
+        icon: 'favicon.ico',
+        properties: {
+          type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+          operator: 'xor',
+          defaultValues: {},
+        } satisfies BinaryOperatorNodeProperties,
+      },
+      {
+        type: BinaryOperatorNodeType,
+        label: '相等xnor',
+        icon: 'favicon.ico',
+        properties: {
+          type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+          operator: 'xnor',
+          defaultValues: {},
+        } satisfies BinaryOperatorNodeProperties,
+      },
+    ];
+    if (direction === 'in') {
+      booleanTypeResult.push(...comparisonDragNodeConfig);
+    }
+    return booleanTypeResult;
+  } //布尔型处理结束
+
+  // 字符串型
+  if (anchorType.basicTypeName === BUILTIN_BASIC_STRING_TYPE) {
+    const stringTypeResult: LogicFlow.OnDragNodeConfig[] = [
+      {
+        type: BinaryOperatorNodeType,
+        label: '加法',
+        icon: 'favicon.ico',
+        properties: {
+          type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+          operator: 'addition',
+          defaultValues: {},
+        } satisfies BinaryOperatorNodeProperties,
+      },
+    ];
+    if (direction === 'out') {
+      stringTypeResult.push(...comparisonDragNodeConfig);
+    }
+    return stringTypeResult;
+  } //字符串型处理结束
+
   const recommendations: LogicFlow.OnDragNodeConfig[] = [
     {
       type: BinaryOperatorNodeType,
@@ -132,19 +282,30 @@ export function binaryOperatorNodeGenerateAnchorRecommendation(
         defaultValues: {},
       } satisfies BinaryOperatorNodeProperties,
     },
-  ];
-
-  if (anchorType.basicTypeName === BUILTIN_BASIC_INTEGER_TYPE) {
-    recommendations.push({
+    {
       type: BinaryOperatorNodeType,
-      label: '整除',
+      label: anchorType.basicTypeName === BUILTIN_BASIC_INTEGER_TYPE ? '整除' : '除法',
       icon: 'favicon.ico',
       properties: {
         defaultValues: {},
         type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
-        operator: 'floor_division',
+        operator:
+          anchorType.basicTypeName === BUILTIN_BASIC_INTEGER_TYPE ? 'floor_division' : 'division',
       } satisfies BinaryOperatorNodeProperties,
-    });
+    },
+    {
+      type: BinaryOperatorNodeType,
+      label: '乘方',
+      icon: 'favicon.ico',
+      properties: {
+        type: anchorType.toString(), // 在 LogicFlow 中，锚点类型必须是字符串
+        operator: 'exponentiation',
+        defaultValues: {},
+      } satisfies BinaryOperatorNodeProperties,
+    },
+  ];
+
+  if (anchorType.basicTypeName === BUILTIN_BASIC_INTEGER_TYPE) {
     recommendations.push({
       type: BinaryOperatorNodeType,
       label: '取余运算',
@@ -155,6 +316,10 @@ export function binaryOperatorNodeGenerateAnchorRecommendation(
         operator: 'modulus',
       } satisfies BinaryOperatorNodeProperties,
     });
+  }
+
+  if (direction === 'out') {
+    recommendations.push(...comparisonDragNodeConfig);
   }
 
   return recommendations;
